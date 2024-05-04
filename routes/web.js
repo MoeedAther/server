91e972fea671fc1c7d0db1a80daaaa571c2bca67
Moeed Ather <softwareengineermoeed@gmail.com>
2024-05-04 14:13:21 +0500

    5th commit

--- routes/web.js
+++ routes/web.js
@@ -4,6 +4,9 @@ import offerController from '../controllers/offerController.js';
 
 const router=express.Router();
 
+//Home Page Price Controller
+router.post('/pricecheck', offerController.offers)
+
 //Offer Controller
 router.post('/offers', offerController.offers)
 
