b19afce6a56eee871943bab59379a40aa0c59370
Moeed Ather <softwareengineermoeed@gmail.com>
2024-06-22 17:08:11 +0500

    11th commit

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,11 +1,25 @@
 
 import mysql from "mysql";
+import dotenv from 'dotenv';
 
+dotenv.config(process.env.HOST, );
+
+console.log(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_DATABASE)
     const db=mysql.createConnection({
-        host:'localhost',
-        user:"root",
-        password:"macbook",
-        database:"coinoswap"
+        // host:`${process.env.HOST}`,
+        // user:`${process.env.USER}`,
+        // password:`${process.env.PASSWORD}`, 
+        // database:`${process.env.DATABASE}`
+
+        // host:`${process.env.HOST}`,
+        // user:`${process.env.USER}`,
+        // password:`${process.env.PASSWORD}`, 
+        // database:`${process.env.DATABASE}`
+
+        host:process.env.DB_HOST,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD, 
+        database:process.env.DB_DATABASE
     
     })
 
